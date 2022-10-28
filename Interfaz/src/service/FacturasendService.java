d1a40f6c6391245a15f1c09d3ba1e78ad6463168
LucasF <lucasf@tips.com.py>
2022-10-28 12:02:04 -0300

    Cambio de lugar de codigo de visualizacion de tabla

--- Interfaz/src/service/FacturasendService.java
+++ Interfaz/src/service/FacturasendService.java
@@ -30,6 +30,7 @@ import bean.Cliente;
 import bean.DocumentoElectronico;
 import connect.BDConnect;
 import core.Core;
+import views.table.TableDesign;
 
 public class FacturasendService {
 
@@ -79,6 +80,7 @@ public class FacturasendService {
 		}
 	}
 	
+	TableDesign tb = new TableDesign();
 	public Integer populateTransactionTable(JTable table, String q, Integer tipoDocumento, Integer page, Integer size){
 		Integer retorno = 0;
 		Object [] titulos = {null,"Mov #", "Fecha","Cliente","N° Factura","Moneda", "Total", "Estado"};
@@ -98,7 +100,7 @@ public class FacturasendService {
 				datos[4] = StringUtil.padLeftZeros(rs.get(i).get(Core.getFieldName("establecimiento", readDBProperties()))+"", 3)  + "-" + StringUtil.padLeftZeros(rs.get(i).get(Core.getFieldName("punto", readDBProperties()))+"", 3) + "-" + StringUtil.padLeftZeros(rs.get(i).get(Core.getFieldName("numero", readDBProperties())) + "", 7);
 				datos[5] = rs.get(i).get(Core.getFieldName("moneda", readDBProperties()));
 				datos[6] = rs.get(i).get(Core.getFieldName("total", readDBProperties())) != null ? rs.get(i).get(Core.getFieldName("total", readDBProperties())) : 0;
-				datos[7] = getEstadoValue(0);
+				datos[7] = tb.getEstadoValue(0);
 				
 				model.addRow(datos);
 			}
@@ -107,8 +109,8 @@ public class FacturasendService {
 			e.printStackTrace();
 		}
 		table.setModel(model);
-		setCellAlignPositiion(table);
-		addCheckBox(0, table);
+		tb.setCellAlignPositiion(table);
+		tb.addCheckBox(0, table);
 		table.getColumnModel().getColumn(1).setPreferredWidth(10);
 		table.getColumnModel().getColumn(5).setPreferredWidth(20);
 		
@@ -116,54 +118,7 @@ public class FacturasendService {
 	}
 		
 	
-	private void addCheckBox(int columna, JTable table) {
-		TableColumn tc = table.getColumnModel().getColumn(columna);
-//		tc.setHeaderRenderer(new HeaderRenderer2(table.getTableHeader()));//Esta es la linea que trata de poner el chk pero da error
-		tc.setCellEditor(table.getDefaultEditor(Boolean.class));
-		tc.setCellRenderer(table.getDefaultRenderer(Boolean.class));
-		tc.setPreferredWidth(50);
-	}
 	
-	private String getEstadoValue(int valor) {
-		String returnValue;
-		switch (valor) {
-		case -1:
-			returnValue= "Borrador";
-			break;
-		case 2:
-			returnValue= "Aprobado";
-			break;
-		case 3:
-			returnValue= "Aprobado c/ Error";
-			break;
-		case 4:
-			returnValue= "Rechazado";
-			break;
-		case 98:
-			returnValue= "Inexistente";
-			break;
-		case 99:
-			returnValue= "Cancelado";
-			break;
-		default:
-			returnValue= "Desconocido";
-			break;
-		}
-		return returnValue;
-	}
-	
-	private void setCellAlignPositiion(JTable table) {
-		final DefaultTableCellRenderer defaultTableCellRenderer = new DefaultTableCellRenderer();
-		defaultTableCellRenderer.setHorizontalTextPosition(0);
-		table.setDefaultRenderer(getClass(), defaultTableCellRenderer);
-		table.getColumnModel().getColumn(1).setCellRenderer(defaultTableCellRenderer);
-		table.getColumnModel().getColumn(2).setCellRenderer(defaultTableCellRenderer);
-		table.getColumnModel().getColumn(3).setCellRenderer(defaultTableCellRenderer);
-		table.getColumnModel().getColumn(4).setCellRenderer(defaultTableCellRenderer);
-		table.getColumnModel().getColumn(5).setCellRenderer(defaultTableCellRenderer);
-		table.getColumnModel().getColumn(6).setCellRenderer(new CurrencyCellRenderer());
-		table.getColumnModel().getColumn(7).setCellRenderer(new CeldaPersonalizada());
-	}
 	
 	static ConfigProperties configProperties;
 	public static Map readDBProperties() {
@@ -175,152 +130,6 @@ public class FacturasendService {
 	}
 }
 
-class CurrencyCellRenderer extends DefaultTableCellRenderer {
-	 
-    private static final NumberFormat FORMAT = NumberFormat.getCurrencyInstance();
-
-    @Override
-    public final Component getTableCellRendererComponent(JTable table, Object value,
-            boolean isSelected, boolean hasFocus, int row, int column) {
-        final Component result = super.getTableCellRendererComponent(table, value,
-                isSelected, hasFocus, row, column);
-        if (value instanceof Number) {
-            //setHorizontalAlignment(JLabel.RIGHT);
-            setText(FORMAT.format(value));
-        } else {
-            setText("");
-        }
-        return result;
-    }
-}
-
-class CeldaPersonalizada extends DefaultTableCellRenderer {
-	 
-	Font font = new Font("helvetica", Font.PLAIN, 12);
-	Map  attributes = font.getAttributes();
-
-	@Override
-	public Component getTableCellRendererComponent(JTable table, Object value,
-            boolean isSelected, boolean hasFocus, int row, int column) {
-		Component componente = super.getTableCellRendererComponent(table, value,
-                isSelected, hasFocus, row, column);
-		if(column == 7) {
-			//String estadoStr = this.getText().toString();
-			switch (this.getText()) {
-			case "Borrador":
-				componente.setBackground(Color.white);
-				componente.setForeground(Color.gray);
-				break;
-			case  "Aprobado":
-				componente.setBackground(Color.green);
-				componente.setForeground(Color.black);
-				break;
-			case "Aprobado c/ Error":
-				componente.setBackground(Color.green);
-				componente.setForeground(Color.black);
-				//resaltar o algo
-				attributes.put(TextAttribute.STRIKETHROUGH, TextAttribute.STRIKETHROUGH_ON); 
-				componente.setFont( new Font(attributes));
-				break;
-			case "Rechazado":
-				componente.setBackground(Color.red);
-				componente.setForeground(Color.white);
-				break;
-			case "Inexistente":
-				componente.setBackground(Color.white);
-				componente.setForeground(Color.black);
-				//preguntar al profe
-				attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON); 
-				componente.setFont( new Font(attributes));
-				break;
-			case "Cancelado":
-				componente.setBackground(Color.red);
-				componente.setForeground(Color.white);
-				//tachar
-				attributes.put(TextAttribute.STRIKETHROUGH, TextAttribute.STRIKETHROUGH_ON); 
-				componente.setFont( new Font(attributes));
-				break;
-			case "Desconocido":
-				componente.setBackground(Color.white);
-				componente.setForeground(Color.black);
-				break;
-			}
-		}
-		return componente;
-		
-	}
-}
-
-class HeaderRenderer2 implements TableCellRenderer {
-
-private final JCheckBox check = new JCheckBox();
-
-public HeaderRenderer2(JTableHeader header) {
-    check.setOpaque(false);
-    check.setFont(header.getFont());
-    header.addMouseListener(new MouseAdapter() {
-
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            JTable table = ((JTableHeader) e.getSource()).getTable();
-            TableColumnModel columnModel = table.getColumnModel();
-            int viewColumn = columnModel.getColumnIndexAtX(e.getX());
-            int modelColumn = table.convertColumnIndexToModel(viewColumn);
-            if (modelColumn == 0) {
-                check.setSelected(!check.isSelected());
-                TableModel m = table.getModel();
-                Boolean f = check.isSelected();
-                for (int i = 0; i < m.getRowCount(); i++) {
-                    m.setValueAt(f, i, 0);
-                }
-                ((JTableHeader) e.getSource()).repaint();
-            }
-        }
-    });
-}
-
-@Override
-public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
-		int row, int column) {
-	// TODO Auto-generated method stub
-	return null;
-}
-/*
-@Override
-public Component getTableCellRendererComponent(
-        JTable tbl, Object val, boolean isS, boolean hasF, int row, int col) {
-    TableCellRenderer r = tbl.getTableHeader().getDefaultRenderer();
-    //JLabel l = (JLabel) r.getTableCellRendererComponent(tbl, val, isS, hasF, row, col);
-    //l.setIcon(new CheckBoxIcon(check));
-    return l;
-}
-private static class CheckBoxIcon implements Icon {
-
-    private final JCheckBox check;
-
-    public CheckBoxIcon(JCheckBox check) {
-        this.check = check;
-    }
-
-    @Override
-    public int getIconWidth() {
-        return check.getPreferredSize().width;
-    }
-
-    @Override
-    public int getIconHeight() {
-        return check.getPreferredSize().height;
-    }
-
-    @Override
-    public void paintIcon(Component c, Graphics g, int x, int y) {
-        SwingUtilities.paintComponent(
-                g, check, (Container) c, x, y, getIconWidth(), getIconHeight());
-    }
-}*/
-
-
-}
 
 
 
---
+++ Interfaz/src/views/table/TableDesign.java
@@ -0,0 +1,185 @@
+package views.table;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.font.TextAttribute;
+import java.text.NumberFormat;
+import java.util.Map;
+
+import javax.swing.JCheckBox;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+import javax.swing.table.TableModel;
+
+
+public class TableDesign {
+
+	public void addCheckBox(int columna, JTable table) {
+		TableColumn tc = table.getColumnModel().getColumn(columna);
+//		tc.setHeaderRenderer(new HeaderRenderer2(table.getTableHeader()));//Esta es la linea que trata de poner el chk pero da error
+		tc.setCellEditor(table.getDefaultEditor(Boolean.class));
+		tc.setCellRenderer(table.getDefaultRenderer(Boolean.class));
+		tc.setPreferredWidth(50);
+	}
+	
+	public String getEstadoValue(int valor) {
+		String returnValue;
+		switch (valor) {
+		case -1:
+			returnValue= "Borrador";
+			break;
+		case 2:
+			returnValue= "Aprobado";
+			break;
+		case 3:
+			returnValue= "Aprobado c/ Error";
+			break;
+		case 4:
+			returnValue= "Rechazado";
+			break;
+		case 98:
+			returnValue= "Inexistente";
+			break;
+		case 99:
+			returnValue= "Cancelado";
+			break;
+		default:
+			returnValue= "Desconocido";
+			break;
+		}
+		return returnValue;
+	}
+	
+	public void setCellAlignPositiion(JTable table) {
+		final DefaultTableCellRenderer defaultTableCellRenderer = new DefaultTableCellRenderer();
+		defaultTableCellRenderer.setHorizontalTextPosition(0);
+		table.setDefaultRenderer(getClass(), defaultTableCellRenderer);
+		table.getColumnModel().getColumn(1).setCellRenderer(defaultTableCellRenderer);
+		table.getColumnModel().getColumn(2).setCellRenderer(defaultTableCellRenderer);
+		table.getColumnModel().getColumn(3).setCellRenderer(defaultTableCellRenderer);
+		table.getColumnModel().getColumn(4).setCellRenderer(defaultTableCellRenderer);
+		table.getColumnModel().getColumn(5).setCellRenderer(defaultTableCellRenderer);
+		table.getColumnModel().getColumn(6).setCellRenderer(new CurrencyCellRenderer());
+		table.getColumnModel().getColumn(7).setCellRenderer(new CeldaPersonalizada());
+	}
+}
+class CurrencyCellRenderer extends DefaultTableCellRenderer {
+	 
+    private static final NumberFormat FORMAT = NumberFormat.getCurrencyInstance();
+
+    @Override
+    public final Component getTableCellRendererComponent(JTable table, Object value,
+            boolean isSelected, boolean hasFocus, int row, int column) {
+        final Component result = super.getTableCellRendererComponent(table, value,
+                isSelected, hasFocus, row, column);
+        if (value instanceof Number) {
+            //setHorizontalAlignment(JLabel.RIGHT);
+            setText(FORMAT.format(value));
+        } else {
+            setText("");
+        }
+        return result;
+    }
+}
+
+class CeldaPersonalizada extends DefaultTableCellRenderer {
+	 
+	Font font = new Font("helvetica", Font.PLAIN, 12);
+	Map  attributes = font.getAttributes();
+
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value,
+            boolean isSelected, boolean hasFocus, int row, int column) {
+		Component componente = super.getTableCellRendererComponent(table, value,
+                isSelected, hasFocus, row, column);
+		if(column == 7) {
+			//String estadoStr = this.getText().toString();
+			switch (this.getText()) {
+			case "Borrador":
+				componente.setBackground(Color.white);
+				componente.setForeground(Color.gray);
+				break;
+			case  "Aprobado":
+				componente.setBackground(Color.green);
+				componente.setForeground(Color.black);
+				break;
+			case "Aprobado c/ Error":
+				componente.setBackground(Color.green);
+				componente.setForeground(Color.black);
+				//resaltar o algo
+				attributes.put(TextAttribute.STRIKETHROUGH, TextAttribute.STRIKETHROUGH_ON); 
+				componente.setFont( new Font(attributes));
+				break;
+			case "Rechazado":
+				componente.setBackground(Color.red);
+				componente.setForeground(Color.white);
+				break;
+			case "Inexistente":
+				componente.setBackground(Color.white);
+				componente.setForeground(Color.black);
+				//preguntar al profe
+				attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON); 
+				componente.setFont( new Font(attributes));
+				break;
+			case "Cancelado":
+				componente.setBackground(Color.red);
+				componente.setForeground(Color.white);
+				//tachar
+				attributes.put(TextAttribute.STRIKETHROUGH, TextAttribute.STRIKETHROUGH_ON); 
+				componente.setFont( new Font(attributes));
+				break;
+			case "Desconocido":
+				componente.setBackground(Color.white);
+				componente.setForeground(Color.black);
+				break;
+			}
+		}
+		return componente;
+		
+	}
+}
+
+class HeaderRenderer2 implements TableCellRenderer {
+
+private final JCheckBox check = new JCheckBox();
+
+public HeaderRenderer2(JTableHeader header) {
+    check.setOpaque(false);
+    check.setFont(header.getFont());
+    header.addMouseListener(new MouseAdapter() {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            JTable table = ((JTableHeader) e.getSource()).getTable();
+            TableColumnModel columnModel = table.getColumnModel();
+            int viewColumn = columnModel.getColumnIndexAtX(e.getX());
+            int modelColumn = table.convertColumnIndexToModel(viewColumn);
+            if (modelColumn == 0) {
+                check.setSelected(!check.isSelected());
+                TableModel m = table.getModel();
+                Boolean f = check.isSelected();
+                for (int i = 0; i < m.getRowCount(); i++) {
+                    m.setValueAt(f, i, 0);
+                }
+                ((JTableHeader) e.getSource()).repaint();
+            }
+        }
+    });
+}
+
+@Override
+public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+		int row, int column) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+}
