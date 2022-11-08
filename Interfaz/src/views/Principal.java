f36bc98e15266fa000be04282c9b139f43943c29
LucasF <lucasf@tips.com.py>
2022-11-08 11:29:34 -0300

    agg filtro de tabla y pestanha otros en config

--- Interfaz/src/views/Principal.java
+++ Interfaz/src/views/Principal.java
@@ -19,6 +19,8 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.SoftBevelBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.table.DefaultTableModel;
 
 import com.google.gson.Gson;
@@ -38,6 +40,7 @@ import javax.swing.border.EtchedBorder;
 import javax.swing.ImageIcon;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
@@ -47,6 +50,11 @@ import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JMenu;
+import javax.swing.JSeparator;
 
 public class Principal extends JFrame {
 
@@ -85,10 +93,19 @@ public class Principal extends JFrame {
 	InfoMovimiento movDetails;
 	Paginacion paginacion;
 	FacturasendService fs;
-	private JComboCheckBox comboCheck;
 	
 	private Integer rowsPerPage = 10;
 	private Integer tipoDocumento = 1;
+	private JMenuBar menuBar;
+	private JMenu mnFiltrosDeTabla;
+	private JCheckBoxMenuItem chkMenuMovNro;
+	private JCheckBoxMenuItem chkMenuCliente;
+	private JCheckBoxMenuItem chkMenuFecha;
+	private JCheckBoxMenuItem chkMenuNroFactura;
+	private JCheckBoxMenuItem chkMenuMoneda;
+	private JCheckBoxMenuItem chkMenuTotal;
+	private JCheckBoxMenuItem chkMenuEstado;
+	private JCheckBoxMenuItem chkMenuCdc;
 	/**
 	 * Launch the application.
 	 */
@@ -119,8 +136,6 @@ public class Principal extends JFrame {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		
-		comboCheck = new JComboCheckBox();
 		
 		if (FacturasendService.readDBProperties().get("database.rows_per_page") != null) {
 			rowsPerPage = Integer.valueOf(FacturasendService.readDBProperties().get("database.rows_per_page")+"");	
@@ -210,6 +225,7 @@ public class Principal extends JFrame {
 		
 		btnBuscar = new JButton("");
 		btnBuscar.setIcon(new ImageIcon(Principal.class.getResource("/resources/search.png")));
+		
 		
 		
 		btnFacturas = new JButton("Facturas");
@@ -337,13 +353,136 @@ public class Principal extends JFrame {
 
 		paneCenter.add(paneSouthTable, BorderLayout.SOUTH);
 		paneSouthTable.add(paginacion, BorderLayout.EAST);
-//		
-//		paneSouthTableCenter = new JPanel();
-//		paneSouthTable.add(paneSouthTableCenter);
+		
+		menuBar = new JMenuBar();
+		frmFacturaSend.setJMenuBar(menuBar);
+		
+		mnFiltrosDeTabla = new JMenu("Filtros de Tabla");
+		menuBar.add(mnFiltrosDeTabla);
+		
+		chkMenuMovNro = new JCheckBoxMenuItem("Mov #");
+		
+		
+		mnFiltrosDeTabla.add(chkMenuMovNro);
+		
+		JSeparator separator = new JSeparator();
+		mnFiltrosDeTabla.add(separator);
+		
+		chkMenuFecha = new JCheckBoxMenuItem("Fecha");
+		mnFiltrosDeTabla.add(chkMenuFecha);
+		
+		JSeparator separator_1 = new JSeparator();
+		mnFiltrosDeTabla.add(separator_1);
+		
+		chkMenuCliente = new JCheckBoxMenuItem("Cliente");
+		mnFiltrosDeTabla.add(chkMenuCliente);
+		
+		JSeparator separator_2 = new JSeparator();
+		mnFiltrosDeTabla.add(separator_2);
+		
+		chkMenuNroFactura = new JCheckBoxMenuItem("Nro. Factura");
+		mnFiltrosDeTabla.add(chkMenuNroFactura);
+		
+		JSeparator separator_3 = new JSeparator();
+		mnFiltrosDeTabla.add(separator_3);
+		
+		chkMenuMoneda = new JCheckBoxMenuItem("Moneda");
+		mnFiltrosDeTabla.add(chkMenuMoneda);
+		
+		JSeparator separator_4 = new JSeparator();
+		mnFiltrosDeTabla.add(separator_4);
+		
+		chkMenuTotal = new JCheckBoxMenuItem("Total");
+		mnFiltrosDeTabla.add(chkMenuTotal);
+		
+		JSeparator separator_5 = new JSeparator();
+		mnFiltrosDeTabla.add(separator_5);
+		
+		chkMenuEstado = new JCheckBoxMenuItem("Estado");
+		mnFiltrosDeTabla.add(chkMenuEstado);
+		
+		JSeparator separator_6 = new JSeparator();
+		mnFiltrosDeTabla.add(separator_6);
+		
+		chkMenuCdc = new JCheckBoxMenuItem("CDC");
+		mnFiltrosDeTabla.add(chkMenuCdc);
 		
 	}
 	
 	private void events() {
+		chkMenuMovNro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(chkMenuMovNro.isSelected()) {
+					ocultarColumnas(jTableTransaction, 0);
+				}else {
+					mostrarColumnas(jTableTransaction, 0);
+				}
+			}
+		});
+		chkMenuFecha.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(chkMenuFecha.isSelected()) {
+					ocultarColumnas(jTableTransaction, 1);
+				}else {
+					mostrarColumnas(jTableTransaction, 1);
+				}
+			}
+		});
+		chkMenuCliente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(chkMenuCliente.isSelected()) {
+					ocultarColumnas(jTableTransaction, 2);
+				}else {
+					mostrarColumnas(jTableTransaction, 2);
+				}
+			}
+		});
+		chkMenuNroFactura.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(chkMenuNroFactura.isSelected()) {
+					ocultarColumnas(jTableTransaction, 3);
+				}else {
+					mostrarColumnas(jTableTransaction, 3);
+				}
+			}
+		});
+		chkMenuMoneda.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(chkMenuMoneda.isSelected()) {
+					ocultarColumnas(jTableTransaction, 4);
+				}else {
+					mostrarColumnas(jTableTransaction, 4);
+				}
+			}
+		});
+		chkMenuTotal.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(chkMenuTotal.isSelected()) {
+					ocultarColumnas(jTableTransaction, 5);
+				}else {
+					mostrarColumnas(jTableTransaction, 5);
+				}
+			}
+		});
+		chkMenuEstado.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(chkMenuEstado.isSelected()) {
+					ocultarColumnas(jTableTransaction, 6);
+				}else {
+					mostrarColumnas(jTableTransaction, 6);
+				}
+			}
+		});
+		chkMenuCdc.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(chkMenuCdc.isSelected()) {
+					ocultarColumnas(jTableTransaction, 7);
+				}else {
+					mostrarColumnas(jTableTransaction, 7);
+				}
+			}
+		});
+		
 		btnNoEnviar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -605,4 +744,24 @@ public class Principal extends JFrame {
 		return paginacion;
 	}
 	
+	public void ocultarColumnas (JTable table, int col) {
+		table.getColumnModel().getColumn(col).setMaxWidth(0);
+		table.getColumnModel().getColumn(col).setMinWidth(0);
+		table.getTableHeader().getColumnModel().getColumn(col).setMaxWidth(0);
+		table.getTableHeader().getColumnModel().getColumn(col).setMinWidth(0);
+	}
+	
+	public void mostrarColumnas(JTable table, int col) {
+		int colPreferredWidth  =  col == 0?60:col==1?80:col==2?280:col==3?65:col==4?50:col==5?65:col ==6?80:100;
+		int colHeaderPreferredWidth = col == 0?60:col==1?80:col==2?280:col==3?65:col==4?50:col==5?65:col ==6?80:100;
+		
+		
+		table.getColumnModel().getColumn(col).setMaxWidth(200);
+		table.getColumnModel().getColumn(col).setMinWidth(0);
+		table.getColumnModel().getColumn(col).setPreferredWidth(colPreferredWidth);
+		table.getTableHeader().getColumnModel().getColumn(col).setMaxWidth(200);
+		table.getTableHeader().getColumnModel().getColumn(col).setMinWidth(0);
+		table.getTableHeader().getColumnModel().getColumn(col).setPreferredWidth(colHeaderPreferredWidth);
+
+	}
 }
