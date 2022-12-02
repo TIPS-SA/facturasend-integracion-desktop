8944eb5cc288fcbd0effe41de283352a5a0e3515
LucasF <lucasf@tips.com.py>
2022-12-02 13:28:34 -0300

    mejora visual del xmlviewer

---
+++ Interfaz/src/service/xml/XmlViewFactory.java
@@ -0,0 +1,17 @@
+package service.xml;
+
+import javax.swing.text.Element;
+import javax.swing.text.View;
+import javax.swing.text.ViewFactory;
+
+public class XmlViewFactory extends Object implements ViewFactory {
+
+    /**
+     * @see javax.swing.text.ViewFactory#create(javax.swing.text.Element)
+     */
+    public View create(Element element) {
+
+        return new XmlView(element);
+    }
+
+}
--- Interfaz/src/views/ShowXMLDialog.java
+++ Interfaz/src/views/ShowXMLDialog.java
@@ -9,12 +9,30 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import service.xml.XmlTextPane;
+
 import javax.swing.JScrollPane;
-import javax.swing.JEditorPane;
 import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.ByteArrayInputStream;
+import java.io.StringWriter;
 import java.awt.event.ActionEvent;
 
 public class ShowXMLDialog extends JDialog {
@@ -25,6 +43,7 @@ public class ShowXMLDialog extends JDialog {
 	private JPanel buttonPane;
 	private JScrollPane scrollPane;
 	private JTextArea txtAXML;
+	private XmlTextPane xmlTextPane;
 	private KeyboardFocusManager kb;
 
 	/**
@@ -47,7 +66,7 @@ public class ShowXMLDialog extends JDialog {
 	public ShowXMLDialog(String xml) {
 		setModal(true);
 		this.xml = xml;
-		setBounds(100, 100, 400, 600);
+		setBounds(100, 100, 500, 650);
 		init(xml);
 		events();
 	}
@@ -65,8 +84,15 @@ public class ShowXMLDialog extends JDialog {
 				txtAXML = new JTextArea();
 				txtAXML.setLineWrap(true);
 				txtAXML.setEditable(false);
-				txtAXML.setText(xml);
-				scrollPane.setViewportView(txtAXML);
+				
+				
+				txtAXML.setText(toPrettyString(xml,2));
+				
+				xmlTextPane = new XmlTextPane();
+				xmlTextPane.setEditable(false);
+				xmlTextPane.setText(toPrettyString(xml,2));
+				
+				scrollPane.setViewportView(xmlTextPane);
 			}
 		}
 		{
@@ -98,5 +124,41 @@ public class ShowXMLDialog extends JDialog {
 			}
 		});
 		
+	}
+	
+	public static String toPrettyString(String xml, int indent) {
+	    try {
+	        // Turn xml string into a document
+	        Document document = DocumentBuilderFactory.newInstance()
+	                .newDocumentBuilder()
+	                .parse(new InputSource(new ByteArrayInputStream(xml.getBytes("utf-8"))));
+
+	        // Remove whitespaces outside tags
+	        document.normalize();
+	        XPath xPath = XPathFactory.newInstance().newXPath();
+	        NodeList nodeList = (NodeList) xPath.evaluate("//text()[normalize-space()='']",
+	                                                      document,
+	                                                      XPathConstants.NODESET);
+
+	        for (int i = 0; i < nodeList.getLength(); ++i) {
+	            Node node = nodeList.item(i);
+	            node.getParentNode().removeChild(node);
+	        }
+
+	        // Setup pretty print options
+	        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	        transformerFactory.setAttribute("indent-number", indent);
+	        Transformer transformer = transformerFactory.newTransformer();
+	        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+	        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+	        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+	        // Return pretty print xml string
+	        StringWriter stringWriter = new StringWriter();
+	        transformer.transform(new DOMSource(document), new StreamResult(stringWriter));
+	        return stringWriter.toString();
+	    } catch (Exception e) {
+	        throw new RuntimeException(e);
+	    }
 	}
 }
